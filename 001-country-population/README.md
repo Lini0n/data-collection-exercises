632c6f0cc8e768de1e7c47ef93ce20f40bb401a4
Toomas Sõukand <toomas.soukand@pipedrive.com>
2021-04-08 19:00:01 +0300

    country data instead of united states

---
+++ 001-country-population/README.md
@@ -0,0 +1,3 @@
+This data has population numbers between years 1960-2020 for contries
+* India
+* United states
---
+++ 001-country-population/country-population.js
@@ -0,0 +1,27 @@
+const data = require('./united-states-population.json');
+
+// We need a number, how many people lived in india in year 1990.
+function indiaPopulationInYear1990() {
+	throw new Error('Not implemented');
+}
+
+// How many more people lived in india in 2020 than united states.
+function howManyMoreLivedIndiaVsUsIn2020() {
+	throw new Error('Not implemented');
+}
+
+// Over all the provided years, how many people lived in united states on avarage.
+function avarageUsPopulationFromAllPeriod() {
+	throw new Error('Not implemented');
+}
+
+// How many people live in India between years 1990-2000 on avarage.
+function avarageUsPopulationBetweenYear1990To2000() {
+	throw new Error('Not implemented');
+}
+
+module.exports = {
+	usPopulationInYear1990,
+	avarageUsPopulationOverAllData,
+	avarageUsPopulationBetweenYear1990To2000,
+};
