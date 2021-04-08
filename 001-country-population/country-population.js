f77c6c8212f8e6c58c7d09d97a68ce781224ecb5
Lini0n <34913847+Lini0n@users.noreply.github.com>
2021-04-08 20:45:39 +0300

    howManyMoreMillionPeopleLivedIndiaVsUsIn2010

--- 001-country-population/country-population.js
+++ 001-country-population/country-population.js
@@ -12,7 +12,23 @@ function indiaPopulationInYear1990() {
 
 // How many more million people lived in india in 2010 than united states.
 function howManyMoreMillionPeopleLivedIndiaVsUsIn2010() {
-	throw new Error('Not implemented');
+	let usaPopulation2010 = 0;
+	let indPopulation2010 = 0;
+
+	for (const countryYearPopulation of data){
+		if(countryYearPopulation.date ==="2010"){
+			if(countryYearPopulation.countryiso3code === "USA"){
+				usaPopulation2010 = countryYearPopulation.value;
+			}
+			if(countryYearPopulation.countryiso3code === "IND"){
+				indPopulation2010 = countryYearPopulation.value;
+			}
+		}
+	}
+
+	const indMinusUsapopulation = Math.round((indPopulation2010 - usaPopulation2010)/ 1000 / 1000);
+
+	return indMinusUsapopulation;
 }
 
 // Over all the provided years, how many people lived in united states on avarage.
