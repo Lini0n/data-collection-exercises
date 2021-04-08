72c1926df0e61a60220804e97fdcb83bd4ab0121
Toomas Sõukand <toomas.soukand@pipedrive.com>
2021-04-08 18:34:48 +0300

    us population excercises

---
+++ 001-united-states-population/united-states-population.js
@@ -0,0 +1,19 @@
+const data = require('./united-states-population.json');
+
+function usPopulationInYear1990() {
+	throw new Error('Not implemented');
+}
+
+function avarageUsPopulationOverAllData() {
+	throw new Error('Not implemented');
+}
+
+function avarageUsPopulationBetweenYear1990To2000() {
+	throw new Error('Not implemented');
+}
+
+module.exports = {
+	usPopulationInYear1990,
+	avarageUsPopulationOverAllData,
+	avarageUsPopulationBetweenYear1990To2000,
+};
