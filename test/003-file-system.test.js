b035216784ece44515795516b57e21e3119522d1
Toomas Sõukand <toomas.soukand@pipedrive.com>
2021-04-29 20:48:18 +0300

    callback tests

--- test/003-file-system.test.js
+++ test/003-file-system.test.js
@@ -3,26 +3,32 @@ const fileSystem = require('../003-file-system/file-system');
 
 describe('estlandia-corona-statistics', () => {
 	describe('.getDirectoryTextFileNamesArray()', () => {
-		it('returs all the text files in given directory', () => {
-			const result = fileSystem.getDirectoryTextFileNamesArray();
+		it('returs all the text files in given directory', (done) => {
+			fileSystem.getDirectoryTextFileNamesArray((err, result) => {
+				assert.strictEqual(err, null);
+				assert.deepStrictEqual(result, [
+					'logs-2020.txt',
+					'logs-2021.txt',
+				]);
 
-			assert.deepStrictEqual(result, [
-				'logs-2020.txt',
-				'logs-2021.txt',
-			]);
+				return done();
+			});
 		});
 	});
 
 	describe('.getDirectoryTextFilesAllLogRows()', () => {
-		it('returns unique corona statuses in alphaphetical order', () => {
-			const result = fileSystem.getDirectoryTextFilesAllLogRowsArray();
+		it('returns unique corona statuses in alphaphetical order', (done) => {
+			fileSystem.getDirectoryTextFilesAllLogRowsArray((err, result) => {
+				assert.strictEqual(err, null);
+				assert.deepStrictEqual(result, [
+					'INFO: starting server',
+					'INFO: server started',
+					'INFO: api server closing',
+					'INFO: api server closed',
+				]);
 
-			assert.deepStrictEqual(result, [
-				'INFO: starting server',
-				'INFO: server started',
-				'INFO: api server closing',
-				'INFO: api server closed',
-			]);
+				return done();
+			});
 		});
 	});
 });
